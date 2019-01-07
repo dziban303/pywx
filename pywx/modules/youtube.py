89fc0801bbaa5fa6667da8a04716797e161d0968
Dziban303 <dziban303@users.noreply.github.com>
2019-01-07 15:06:23 -0600

    Update youtube.py

--- pywx/modules/youtube.py
+++ pywx/modules/youtube.py
@@ -6,7 +6,6 @@ from .base import ParserCommand
 from registry import register_parser
 
 
-
 def pretty_iso_duration(iso_duration):
     dd = {}
     num = 0
@@ -71,3 +70,4 @@ class YoutubeParser(ParserCommand):
             duration = pretty_iso_duration(video['contentDetails']['duration'])
             lines.append("YOUTUBE: %s [%s]" % (title, duration))
         return lines
+
